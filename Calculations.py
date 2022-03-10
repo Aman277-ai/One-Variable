95437037a08ca490303db1d8d772e644c5ef28f4
Aman277-ai <amanxgupta1@gmail.com>
2022-03-10 22:09:23 +0530

    Done

--- Calculations.py
+++ Calculations.py
@@ -1,30 +1,51 @@
 def calculations(spaceRemovedStr : str):
     var = variable(spaceRemovedStr)
     leftSide = spaceRemovedStr[:spaceRemovedStr.find('=')]
-    rightSide = spaceRemovedStr[spaceRemovedStr.find('=') + 1]
-
-    numOfVar : str = ""
-    leftSideTemp = leftSide[:leftSide.find(var) + 1 : -1]
+    rightSide = spaceRemovedStr[spaceRemovedStr.find('=') + 1:len(spaceRemovedStr)]
     
+    leftSideTemp = leftSide[0 : leftSide.find(var) + 1]
+    leftSideTemp = leftSideTemp[::-1]
+    operatorOfVar : str = ""
+    
+    for x in leftSideTemp:
+        if x == '+':
+            operatorOfVar = '+'
+            break
+        elif x == '-':
+            operatorOfVar = '-'
+            break
+        elif x == '*':
+            operatorOfVar = '*'
+            break
+        elif x == '/':
+            operatorOfVar = '/'
+            break
+    
+    numOfVar : str = leftSideTemp[leftSideTemp.find(var) + 1 : leftSideTemp.find(operatorOfVar)]
+    print(leftSideTemp.find(operatorOfVar))
     print(numOfVar)
+    answer : float = None
 
-    if numOfVar != '':
-        leftSide = leftSide.replace(var, "")
+    if operatorOfVar == '+' or operatorOfVar == '-':
+        if numOfVar == "":
+            leftSide = leftSide.replace(var, "0")
+            answer = (eval(rightSide) - eval(leftSide))
+            if eval(leftSide) == 0:
+                answer = answer * -1
+        else:
+            leftSide = leftSide.replace(var, "")
+            answer = (eval(rightSide) - eval(leftSide))
+            if eval(leftSide) == 0:
+                answer = answer * -1
 
     else:
-        if leftSide[leftSide.find(var) + 1 : leftSide.find(var) + 2] == '*':
-            leftSide = leftSide.replace(var, '1')
-        
-        elif leftSide[leftSide.find(var) - 1 : leftSide.find(var)] == '/':
-            leftSide = leftSide.replace(var, '1')
-
-        elif leftSide[leftSide.find(var) + 1 : leftSide.find(var) + 2] == '/':
-            leftSide = leftSide.replace(var, '1')
-
+        if numOfVar != "":
+            leftSide = leftSide.replace(var, "")
+            answer = (eval(rightSide) / eval(leftSide))
         else:
-            leftSide = leftSide.replace(var, '0')
-
-    print(leftSide)
+            leftSide = leftSide.replace(var, "1")
+            answer = (eval(rightSide) / eval(leftSide)) 
+    return answer,operatorOfVar
 
 def variable(spaceRemovedStr : str):
     var : str = ""
--- main.py
+++ main.py
@@ -5,4 +5,10 @@ os.system("cls")
 
 inputStr = input("Enter the equation with only one variable:")
 spaceRemovedStr = SpaceRemover.spaceRemover(inputStr)
-answer = Calculations.calculations(spaceRemovedStr)
+operatorOfVar : str = Calculations.calculations(spaceRemovedStr)[1]
+answer = Calculations.calculations(spaceRemovedStr)[0]
+if operatorOfVar == '-':
+            answer *= -1
+if operatorOfVar == '/':
+            answer = 1 / answer
+print(answer)
